bd31a2b218e26fa37d610970b97659c174577cbb
NicolasCanton <73943152+NicolasCanton@users.noreply.github.com>
2021-02-08 19:03:02 +0100

    Update Bingo.java

--- src/Bingo.java
+++ src/Bingo.java
@@ -2,12 +2,12 @@ import java.util.ArrayList;
 
 public class Bingo {
     public static void main(String[] args) {
-        Jugador jugador=new Jugador();
-        Jugador Jugador2=new Jugador();
-        Jugador.rellenar();
-        Jugador2.rellenar();
+        Jugadores jugador=new Jugadores();
+        Jugadores Jugador2=new Jugadores();
+        jugador.completar();
+        Jugador2.completar();
         Generador_de_bolas generar=new Generador_de_bolas();
-         Generador_de_bolas.jugar(Jugador,Jugador2);
+        generar.jugar(jugador,Jugador2);
     }
 
 }
