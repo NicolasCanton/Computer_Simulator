87713e51410dd077db864de932f5173b22077766
NicolasCanton <73943152+NicolasCanton@users.noreply.github.com>
2021-02-08 14:05:56 +0100

    Update and rename Main.java to Bingo.java

---
+++ src/Bingo.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Bingo {
+    public static void main(String[] args) {
+        Jugador jugador=new Jugador();
+        Jugador Jugador2=new Jugador();
+        Jugador.rellenar();
+        Jugador2.rellenar();
+        Generador_de_bolas generar=new Generador_de_bolas();
+         Generador_de_bolas.(Jugador,Jugador2);
+    }
+
+}
